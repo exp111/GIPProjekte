3768995e013d108039090d5267ccfed1b3b953fe
T N <theexp111@gmail.com>
2017-10-21 19:09:17 +0200

    -Organizing -Added P2-1

---
+++ 2-1 (Position im Alphabet)/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Bitte geben Sie den Buchstaben ein : ? ";
+	char buchstabe;
+	cin >> buchstabe;
+	int number = int(buchstabe) - 96;
+	cout << "Der Buchstabe " << buchstabe << " hat die Position " << number << " im Alphabet." << endl;	system("PAUSE");
+	return 0;
+}
