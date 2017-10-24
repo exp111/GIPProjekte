a1d7529351f3d1870cd9b2db3a7c748ae0d4bb87
T N <theexp111@gmail.com>
2017-10-24 12:12:12 +0200

    -Well fuck that

--- 2-1 (Position im Alphabet)/main.cpp
+++ 2-1 (Position im Alphabet)/main.cpp
@@ -5,7 +5,7 @@ int main() {
 	cout << "Bitte geben Sie den Buchstaben ein: ? ";
 	char buchstabe;
 	cin >> buchstabe;
-	int number = int(buchstabe) - (int('a') + 1);
+	int number = int(buchstabe) - 96;
 	cout << "Der Buchstabe " << buchstabe << " hat die Position " << number << " im Alphabet." << endl;
 	system("PAUSE");
 	return 0;
