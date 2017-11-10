400afce71bbc6e6db61f38ec49025c94f43770c8
T N <theexp111@gmail.com>
2017-11-10 08:53:02 +0100

    -Added Offline 4-1 & 4-2 (both finished and tested)

---
+++ 4-1 (Aufzählung rückwarts)/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int zaehler = 50;
+	while (zaehler >= 0)
+	{
+		cout << zaehler << endl;
+		zaehler -= 5;
+	}
+	system("PAUSE");
+	return 0;
+}
