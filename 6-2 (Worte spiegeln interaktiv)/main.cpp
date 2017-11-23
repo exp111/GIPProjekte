c5d61e2575ca8a57b6d47f54ad6cbfc21582b3cd
Exp <theexp111@gmail.com>
2017-11-23 14:06:19 +0100

    -Added 6-3 & Finished 6-2

--- 6-2 (Worte spiegeln interaktiv)/main.cpp
+++ 6-2 (Worte spiegeln interaktiv)/main.cpp
@@ -5,7 +5,31 @@ using namespace std;
 
 int main()
 {
+	string satz;
+	cout << "Bitte geben Sie den Text ein: ? ";
+	getline(cin, satz);
 
+	int cursorPos = 0;
+	char input = 'q';
+	do
+	{
+		//Check Input
+		if (input == 'l' && cursorPos > 0)
+			cursorPos--;
+		else if (input == 'r' && cursorPos < satz.length())
+			cursorPos++;
+		else if (input == 's')
+			wortspiegel(satz, cursorPos);
+
+		cout << endl << satz << endl;
+		for (int i = 0; i < cursorPos; i++)
+			cout << " ";
+		cout << "*" << endl;
+		cout << "Befehl (l: links, r: rechts, s: spiegeln, q: Ende) ?- ";
+		cin >> input;
+	} while (input != 'q');
+
+	cout << endl;
 	system("PAUSE");
 	return 0;
 }
