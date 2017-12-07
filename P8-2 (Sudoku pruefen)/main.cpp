a8168a3b6ada7e87c44415cd1cb4eeabbf2fcd80
T N <theexp111@gmail.com>
2017-12-07 12:27:46 +0100

    -Renamed Arrays and fuck that /shrug

--- P8-2 (Sudoku pruefen)/main.cpp
+++ P8-2 (Sudoku pruefen)/main.cpp
@@ -32,65 +32,65 @@ int main()
 	//Output
 
 	//Spalte
-	int count[9][9] = { 0 };
+	int countSpalte[9][9] = { 0 };
 	for (int i = 0; i < 9; i++)//Zeile
 	{
 		//Check
 		for (int j = 0; j < 9; j++) //Spalte
 		{
-			count[i][sudoku[j][i] - 1]++;
+			countSpalte[i][sudoku[j][i] - 1]++;
 		}
 	
 		//Output
 		for (int j = 0; j < 9; j++) //Zahl
 		{
-			if (count[i][j] == 0)
+			if (countSpalte[i][j] == 0)
 				cout << "Spalte " << i << ": Zahl " << j+1 << " kommt nicht vor." << endl;
 
-			if (count[i][j] > 1)
+			if (countSpalte[i][j] > 1)
 				cout << "Spalte " << i << ": Zahl " << j+1 << " kommt mehrfach vor." << endl;
 		}
 	}
 
 	//Zeile
-	int count2[9][9] = { 0 };
+	int countZeile[9][9] = { 0 };
 	for (int i = 0; i < 9; i++)//Zeile
 	{
 		//Check
 		for (int j = 0; j < 9; j++) //Spalte
 		{
-			count2[i][sudoku[i][j] - 1]++;
+			countZeile[i][sudoku[i][j] - 1]++;
 		}
 	
 		//Output
 		for (int j = 0; j < 9; j++) //Zahl
 		{
-			if (count2[i][j] == 0)
+			if (countZeile[i][j] == 0)
 				cout << "Zeile " << i << ": Zahl " << j + 1 << " kommt nicht vor." << endl;
 
-			if (count2[i][j] > 1)
+			if (countZeile[i][j] > 1)
 				cout << "Zeile " << i << ": Zahl " << j + 1 << " kommt mehrfach vor." << endl;
 		}
 	}
 
 	//Block
-	int count3[9][9] = { 0 };
+	int countBlock[9][9] = { 0 };
 	for (int i = 0; i < 9; i++) //Block
 	{
 		for (int j = (i % 3 * 3); j < (i % 3 * 3 + 3); j++) //Spalte
 		{
 			for (int k = (i / 3 * 3); k < (i / 3 * 3 + 3); k++) //Zeile
 			{
-				count3[i][sudoku[k][j] - 1]++;
+				countBlock[i][sudoku[k][j] - 1]++;
 			}
 		}
 
 		//Output
 		for (int j = 0; j < 9; j++)
 		{
-			if (count3[i][j] == 0)
+			if (countBlock[i][j] == 0)
 				cout << "Block " << i << ": Zahl " << j+1 << " kommt nicht vor." << endl;
-			if (count3[i][j] > 1)
+			if (countBlock[i][j] > 1)
 				cout << "Block " << i << ": Zahl " << j+1 << " kommt mehrfach vor." << endl;
 		}
 	}
